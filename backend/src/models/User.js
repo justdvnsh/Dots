746d8cd3ea2dbdf1960b6ec63f3b9a64d8c1720b
justdvnsh <justdvnsh2208@gmail.com>
2020-04-10 01:57:17 +0530

    added backend - mongoose

---
+++ backend/src/models/User.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    branch: {
+        type: String,
+        required: true
+    },
+    semester: {
+        type: Number,
+        required: true
+    }
+});
+
+UserSchema.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model("User", UserSchema);
---
+++ backend/src/server.js
@@ -0,0 +1,3 @@
+const app = require("express")();
+const db = require("./db");
+
